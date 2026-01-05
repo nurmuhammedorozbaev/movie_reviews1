7968c0f5f230b422c5f4844c4a1ac3d4708d90fb
Nurmuhammed Orozbaev <orozbaevnurik2004@gmail.com>
2026-01-05 20:05:54 +0600

    update project

--- Новая папка/moviereviews/moviereviews/settings.py
+++ Новая папка/moviereviews/moviereviews/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('96a2ba068a67af683f608cecd6778f41')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
